e37243eb08dff2c7a5ebcc2ad0d05181f05aec13
harman <harmandeeps008@gmail.com>
2021-07-17 21:12:52 +0530

    commit

--- server.js
+++ server.js
@@ -8,4 +8,19 @@ server.get("/pets", function (req, res) {
     res.json(data);
   });
 
+server.get("/pettypes", function(req, res) {
+  let typeData = [];
+  let uniquePets = {};
+  for (let i = 0; i < data.length; i++) {
+    let pet = data[i];
+    let petType = pet.animal;
+
+    if (!uniquePets.hasOwnProperty(petType)) {
+      typeData.push(petType);
+      uniquePets[petType] = "true";
+    }
+  }
+  res.json(typeData);
+});
+
 server.listen(4000);
